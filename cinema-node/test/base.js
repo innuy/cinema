856ea2cb8be736e0e63f93cd5cba4b0ca7ecfdd0
Juan Pablo Becoña <juanpablobeco@gmail.com>
2018-12-21 16:33:59 -0300

    Models and tests structure
    -models file structuring
    -tests structuring in folders and files
    -added Mocha and Chai for tests
    -added base.js as an example of tests

---
+++ cinema-node/test/base.js
@@ -0,0 +1,8 @@
+var expect = require("chai").expect;
+
+describe("Base Mocha Test", function() {
+    it("true is true", function() {
+        expect("Go Cinema!!!").to.equal("Go Cinema!!!")
+        expect(1+1).to.equal(2)
+    });
+});
