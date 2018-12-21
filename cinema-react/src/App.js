ec98d470752382c50216af6d20787fa6a3b4c492
Ignacio Duran <ignacio.duran@innuy.com>
2018-12-21 14:37:57 -0300

    #2 Added first styles and components: -Added generic styles -Generated Start component and container

--- cinema-react/src/App.js
+++ cinema-react/src/App.js
@@ -1,20 +1,24 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
+import FirstProcess from "./containers/FirstProcess";
 
 class App extends Component {
   render() {
     return (
         <div>
             <Switch>
-                <Route exact path='/' component={null}/>
+                <Route exact path='/' component={FirstProcess}/>
                 <Route exact path='/login' component={null}/>
                 <Route exact path='/signup' component={null}/>
+                {/*ADMIN SCREENS*/}
                 <Route exact path='/movies' component={null}/>
                 <Route exact path='/auditoriums' component={null}/>
                 <Route exact path='/presentations' component={null}/>
-                <Route exact path='/reservations' component={null}/>
                 <Route exact path='/tickets' component={null}/>
+                {/*USER SCREENS*/}
+                <Route exact path='/reservations' component={null}/>
+
             </Switch>
         </div>
     );
