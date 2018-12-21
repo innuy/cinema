c400ec51e5417559c9ce859f1bce8c06aa2f3f7d
Ignacio Duran <ignacio.duran@innuy.com>
2018-12-21 11:12:11 -0300

    #2 -Added necessary dependencies -Created basic folder structure -Created basic route structure

--- cinema-react/src/App.js
+++ cinema-react/src/App.js
@@ -1,26 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div>
+            <Switch>
+                <Route exact path='/' component={null}/>
+                <Route exact path='/login' component={null}/>
+                <Route exact path='/signup' component={null}/>
+                <Route exact path='/movies' component={null}/>
+                <Route exact path='/auditoriums' component={null}/>
+                <Route exact path='/presentations' component={null}/>
+                <Route exact path='/reservations' component={null}/>
+                <Route exact path='/tickets' component={null}/>
+            </Switch>
+        </div>
     );
   }
 }
