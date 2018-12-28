cf38e045c7b1bc1244228226bf18b9845bb9a973
Juan Pablo Becoña <juanpablobeco@gmail.com>
2018-12-28 16:10:19 -0300

    Add db connection tests and libraries for them #10
    At the start of the tests a connection is established with MongoDB using connectMongo and add a new movie, tests are added to find this element, to test the state of the connection and to test the .env functionality.
    New libraries:
    -now for deployment
    -nodemon for auto running the server
    -sinon for test mocks
    -supertest for http tests

---
+++ cinema-node/test/db/connection.js
@@ -0,0 +1,45 @@
+var expect = require("chai").expect;
+var mongoose = require('mongoose');
+var db = require('../../connectors/mongoDB');
+var Movie = require('../../db/models/movies');
+
+describe("Mongo-Mongoose connection test", function() {
+
+    before(async function () {
+
+        require('dotenv').config();
+        await db.connectMongo()
+            .then(() => {
+                console.log(db.isConnected())
+                const movie = new Movie();
+                movie.name = 'a';
+                movie.save();
+            })
+    });
+
+    after(function() {
+        Movie.deleteOne({ name: 'a'}, function (err) {})
+    });
+
+    it("Print Mongo uri", function() {
+        expect("Go Cinema!!!").to.equal("Go Cinema!!!");
+        expect(process.env.DB_USER).to.equal('JPB');
+    });
+
+    it("Is connected", function () {
+        expect(db.isConnected()).to.equal("connected");
+    });
+
+    it("Find a movie element", async function () {
+        this.retries(2);
+        await Movie.findOne({name: 'a'})
+            .then(movie => {
+                console.log(movie.name);
+                expect(movie.name).to.equal('a');
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    });
+
+});
