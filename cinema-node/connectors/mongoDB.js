f529f4a391c8e3c52cdfe10069264e14dd6a6657
Juan Pablo Becoña <juanpablobeco@gmail.com>
2018-12-28 15:30:00 -0300

    Changes in connectMongo to use promises #10
    In order to start the server after the connection to the database is finished and to avoid the extensive use of callbacks promises are added to connectMongo

--- cinema-node/connectors/mongoDB.js
+++ cinema-node/connectors/mongoDB.js
@@ -2,23 +2,28 @@ var mongoose = require('mongoose');
 
 module.exports.connectMongo = function() {
 
-    //DB Configuration parameters
-    let mongoUri =`mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}`;
-    console.log(mongoUri);
-    mongoose.connect(
-        mongoUri,
-        {
-            useCreateIndex: true,
-            useNewUrlParser: true,
-        },
-    );
+module.exports.connectMongo = () => (
+    new Promise((resolve, reject) => {
+        //DB Configuration parameters
+        let mongoUri =`mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}`;
+        console.log(mongoUri);
+        var mongoDB = mongoose.connect(
+            mongoUri,
+            {
+                useCreateIndex: true,
+                useNewUrlParser: true,
+            },
+        );
+        mongoDB
+            .then(db => {
+                console.log(`Mongo connection established with ${process.env.DB_HOST} host and ${process.env.DB_NAME} database`);
+                resolve(mongoDB);
+            })
+            .catch(err => {
+                console.log(err);
+                reject(err);
+            })
 
-    // Get default connection
-    const db = mongoose.connection;
+    })
+);
 
-    db.on('error', console.error.bind(console, 'Mongo connection error:'));
-
-    console.log(
-        `Mongo connection established with ${process.env.DB_HOST} host and ${process.env.DB_NAME} database`
-    );
-};
