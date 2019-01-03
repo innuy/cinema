9c42818af032ee1b5d5dea0c701e38e96e69a3fe
Juan Pablo Becoña <juanpablobeco@gmail.com>
2019-01-03 18:15:08 -0300

    Refactor of handlers #10
    Extensive refactor of handlers, now all errors are in errors.js

---
+++ cinema-node/routes/movies/errors.js
@@ -0,0 +1,17 @@
+module.exports.databaseError = err => {
+    res.status(500);
+    res.send({
+        "statusCode": 500,
+        "error": "Database error",
+        "message": err
+    });
+};
+
+module.exports.movieNotFound = res => {
+    res.status(404);
+    res.send({
+        "statusCode": 404,
+        "error": "Not Found",
+        "message": "there is no movie here"
+    });
+};
--- cinema-node/routes/movies/handlers.js
+++ cinema-node/routes/movies/handlers.js
@@ -1,53 +1,81 @@
+
+const errors = require("./errors");
 const Movie = require('../../db/models/movies');
 var ObjectID = require('mongodb').ObjectID;
 
 module.exports.create = (req, res) => {
     Movie.create(req.body)
         .then(movie => res.send('Succesfuly created ' + movie.name))
+        .catch(err => errors.databaseError(err))
 };
 
 module.exports.get = (req, res) => {
     Movie.find(req.body)
         .then(movie => res.send(movie))
+        .catch(err => errors.databaseError(err))
 };
 
 module.exports.getById = (req, res) => {
     const id = req.params.id;
-    const details = { '_id': new ObjectID(id) };
-    Movie.find(details)
-        .then(movie => res.send(movie))
+    const id_filter = {'_id': new ObjectID(id)};
+
+    Movie.find(id_filter)
+        .then(movie => {
+            if (thereIsNoMovie(movie)) {
+                errors.movieNotFound(res);
+            } else {
+                res.send(movie);
+            }
+        })
+        .catch(err => errors.databaseError(err))
 };
 
 module.exports.putById = (req, res) => {
-    const id = req.params.id;
-    const details = { '_id': new ObjectID(id) };
+    const id_filter = {'_id': new ObjectID(req.params.id)};
+    const setToReturnUpdatedValue = {new: true};
+    const parametersToSet = {$set: req.body};
 
     Movie.findOneAndUpdate(
-        details,
-        { $set: req.body},
-        { new: true },
+        id_filter,
+        parametersToSet,
+        setToReturnUpdatedValue,
     )
-    .then(movie => res.send(movie))
-    .catch(err => res.send(err))
+        .then(movie => {
+            if (thereIsNoMovie(movie)) {
+                errors.movieNotFound(res);
+            } else {
+                res.send();
+            }
+        })
+        .catch(err => errors.databaseError(err))
 };
 
 module.exports.deleteById = (req, res) => {
-    const id = req.params.id;
-    const details = { '_id': new ObjectID(id) };
-    Movie.find(details)
-        .then(movies =>{
-            if (movies.length >0){
-                Movie.findOneAndDelete(details)
-                    .then(movie => {
-                        res.status(204);
-                        res.send({});
-                    })
-                    .catch(err => res.send(err))
-            }
-            else{
-                res.status(404);
-                res.send({});
+    const id_filter = {'_id': new ObjectID(req.params.id)};
+    Movie.find(id_filter)
+        .then(movies => {
+            if (thereIsNoMovie(movies)) {
+                errors.movieNotFound(res);
+            } else {
+                deleteMovieById(id_filter, res);
             }
         })
-        .catch(err => res.send(err))
+        .catch(err => errors.databaseError(err))
 };
+
+function deleteMovieById(id_filter, res) {
+    Movie.findOneAndDelete(id_filter)
+        .then(movie => {
+            res.status(204);
+            res.send({});
+        })
+        .catch(err => errors.databaseError(err))
+}
+
+function thereIsNoMovie(movie) {
+    if ( Array.isArray(movie))
+        return movie.length === 0;
+    else
+        return movie === null;
+}
+
