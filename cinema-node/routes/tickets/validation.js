4624045fae564f64e7b388f8ee1b0b4883686c95
Juan Pablo Becoña <juanpablobeco@gmail.com>
2019-01-11 11:50:34 -0300

    Ticket index and validation #14
    Declaration of the ticket index and the validation

---
+++ cinema-node/routes/tickets/validation.js
@@ -0,0 +1,51 @@
+const { celebrate, Joi } = require('celebrate');
+Joi.objectId = require('joi-objectid')(Joi);
+
+
+const create = celebrate({
+    body:{
+        presentation: Joi.objectId().required(),
+        seat: Joi.objectId(),
+        seatRow: Joi.number(),
+        seatColumn: Joi.number(),
+    }
+});
+
+const get = celebrate({
+    query: {
+        presentation: Joi.objectId(),
+        seat: Joi.objectId(),
+        seatRow: Joi.number(),
+        seatColumn: Joi.number(),
+    }
+});
+
+const getById = celebrate({
+    params: {
+        id: Joi.objectId(),
+    }
+});
+
+const putById = celebrate({
+    params: {
+        id: Joi.objectId(),
+    },
+    body: {
+        presentation: Joi.objectId().required(),
+        seat: Joi.objectId().required(),
+    }
+});
+
+const deleteById = celebrate({
+    params: {
+        id: Joi.objectId(),
+    }
+});
+
+module.exports = {
+    create,
+    get,
+    getById,
+    putById,
+    deleteById,
+};
