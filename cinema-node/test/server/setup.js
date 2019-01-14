8f1ba6bae0e00da74261300dca25b2e10a3ef4b0
Juan Pablo Becoña <juanpablobeco@gmail.com>
2019-01-14 12:28:10 -0300

    Change isConnected resolve #31
    Change isConnected resolve from 1 to 'connected'

--- cinema-node/test/server/setup.js
+++ cinema-node/test/server/setup.js
@@ -4,7 +4,7 @@ let db
 beforeEach(() => {
     db = require('../../connectors/mongoDB');
     sinon.stub(db, 'connectMongo').resolves("OK");
-    sinon.stub(db, 'isConnected').resolves(1);
+    sinon.stub(db, 'isConnected').resolves('connected');
 });
 
 afterEach(() => {
