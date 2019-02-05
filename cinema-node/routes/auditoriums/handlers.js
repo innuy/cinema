3d4aab31b761798a52f80419bfc5a2363fb14760
Juan Pablo Becoña <juanpablobeco@gmail.com>
2019-02-05 14:44:38 -0300

    Auditorium delete handler refactor #11
    Refactor of auditorium delete handler to improve readability. In accordance to that the test stubbing is adjusted

--- cinema-node/routes/auditoriums/handlers.js
+++ cinema-node/routes/auditoriums/handlers.js
@@ -18,27 +18,6 @@ module.exports.create = (req, res) => {
         });
 };
 
-const createSeats = (auditorium, req) => new Promise((resolve, reject) => {
-    const startingInOne = 1;
-    let seat = {auditorium: auditorium._id};
-    const seatArray = [];
-
-    for (let row = 0; row < req.body.seatRows; row++) {
-        for (let column = 0; column < req.body.seatColumns; column++) {
-            seat.row = row + startingInOne;
-            seat.column = column + startingInOne;
-            Seat.create(seat)
-                .then(seat => {
-                    seatArray.push(seat);
-                })
-                .catch(err => {
-                    reject(err);    //TODO delete already created seats
-                });
-        }
-    }
-    resolve(seatArray);
-});
-
 module.exports.get = (req, res) => {
     Auditorium.find(req.query)
         .then(auditorium => res.send(auditorium))
@@ -63,9 +42,13 @@ module.exports.putById = (req, res) => {
     const id = req.params.id;
     const newAuditorium = req.body;
 
-    checkAuditorium(id, newAuditorium)
+    getAuditorium(id)
         .then(currentAuditorium => {
-            return updateAuditoriumDocument(id, newAuditorium);
+            if (auditoriumSizeUpdated(currentAuditorium, newAuditorium)) {
+                throw(errors.auditoriumChangingSize);
+            } else {
+                return updateAuditoriumDocument(id, newAuditorium);
+            }
         })
         .then(newAuditorium => {
             res.send();
@@ -79,36 +62,68 @@ module.exports.putById = (req, res) => {
         });
 };
 
-function checkAuditorium(id, newAuditorium) {
-    return new Promise((resolve, reject) => {
-        const idFilter = {'_id': new ObjectID(id)};
-        Auditorium.findOne(idFilter)
-            .then(currentAuditorium => {
-                if (thereIsNoAuditorium(currentAuditorium)) {
-                    reject(errors.auditoriumNotFound);
-                } else {
-                    if (auditoriumSizeUpdated(currentAuditorium, newAuditorium)) {
-                        reject(errors.auditoriumChangingSize);
-                    } else {
-                        resolve(currentAuditorium);
-                    }
-                }
-            })
-            .catch(err => reject(err));
-    });
-}
+module.exports.deleteById = (req, res) => {
+    const id = req.params.id;
+    getAuditorium(id)
+        .then(currentAuditorium => {
+            return deleteAuditoriumById(id);
+        })
+        .then(dbresponse => {
+            res.status(204);
+            res.send({});
+        })
+        .catch(err => {
+            if (err instanceof Function) {
+                err(res);
+            } else {
+                errors.databaseError(err, res);
+            }
+        });
+};
 
-function thereIsNoAuditorium(auditorium) {
+const createSeats = (auditorium, req) => new Promise((resolve, reject) => {
+    const startingInOne = 1;
+    let seat = {auditorium: auditorium._id};
+    const seatArray = [];
+
+    for (let row = 0; row < req.body.seatRows; row++) {
+        for (let column = 0; column < req.body.seatColumns; column++) {
+            seat.row = row + startingInOne;
+            seat.column = column + startingInOne;
+            Seat.create(seat)
+                .then(seat => {
+                    seatArray.push(seat);
+                })
+                .catch(err => {
+                    reject(err);    //TODO delete already created seats
+                });
+        }
+    }
+    resolve(seatArray);
+});
+
+const getAuditorium = id => new Promise((resolve, reject) => {
+    const idFilter = {'_id': new ObjectID(id)};
+    Auditorium.findOne(idFilter)
+        .then(currentAuditorium => {
+            if (thereIsNoAuditorium(currentAuditorium)) {
+                reject(errors.auditoriumNotFound);
+            } else {
+                resolve(currentAuditorium);
+            }
+        })
+        .catch(err => reject(err));
+});
+
+const thereIsNoAuditorium = auditorium => {
     if (Array.isArray(auditorium))
         return auditorium.length === 0;
     else
         return auditorium === null;
-}
+};
 
-function auditoriumSizeUpdated(auditorium, newAuditorium) {
-    return (auditorium.seatRows !== newAuditorium.seatRows) ||
-        (auditorium.seatColumns !== newAuditorium.seatColumns);
-}
+const auditoriumSizeUpdated = (auditorium, newAuditorium) => (auditorium.seatRows !== newAuditorium.seatRows) ||
+    (auditorium.seatColumns !== newAuditorium.seatColumns);
 
 const updateAuditoriumDocument = (id, newAuditorium) => new Promise((resolve, reject) => {
     const idFilter = {'_id': new ObjectID(id)};
@@ -129,27 +144,14 @@ const updateAuditoriumDocument = (id, newAuditorium) => new Promise((resolve, re
         .catch(err => reject(err))
 });
 
-module.exports.deleteById = (req, res) => {
+const deleteAuditoriumById = id => new Promise((resolve, reject) => {
     const idFilter = {'_id': new ObjectID(req.params.id)};
-    Auditorium.find(idFilter)
-        .then(auditoriums => {
-            if (thereIsNoAuditorium(auditoriums)) {
-                errors.auditoriumNotFound(res);
-            } else {
-                deleteAuditoriumById(idFilter, res);
-            }
-        })
-        .catch(err => errors.databaseError(err, res))
-};
-
-function deleteAuditoriumById(idFilter, res) {
-    Seat.deleteMany({auditorium: idFilter._id})
-        .catch(err => errors.databaseError(err, res));
+    Seat.deleteMany({auditorium: id})
+        .catch(err => reject(err));
 
     Auditorium.findOneAndDelete(idFilter)
         .then(dbresponse => {
-            res.status(204);
-            res.send({});
+            resolve(dbresponse);
         })
         .catch(err => errors.databaseError(err, res));
-}
+});
