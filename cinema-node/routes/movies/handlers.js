f361aa9094f0b0b3dee5860e5642e945d4789f04
Juan Pablo Becoña <juanpablobeco@gmail.com>
2019-02-07 14:25:22 -0300

    Delete presentations and tickets when deleting a movie #10
    Add a parallel delete promise which deletes all presentation related to the movie and all tickets related to the presentation.
    New tests are add and the old ones updated accordingly.

--- cinema-node/routes/movies/handlers.js
+++ cinema-node/routes/movies/handlers.js
@@ -1,5 +1,9 @@
 const errors = require("./errors");
+
 const Movie = require('../../db/models/movies');
+const Presentation = require("../../db/models/presentations");
+const Ticket = require("../../db/models/tickets");
+
 var ObjectID = require('mongodb').ObjectID;
 
 module.exports.create = (req, res) => {
@@ -50,10 +54,15 @@ module.exports.putById = (req, res) => {
 };
 
 module.exports.deleteById = (req, res) => {
-    movieId = req.params.id;
+    const movieId = req.params.id;
     ifMovieExists(movieId)
-        .then(movie => {
-            return deleteMovieById(movieId);
+        .then(async movies => {
+            const movieFilter = { movie: movieId };
+            const presentationList = await getPresentationListWithFilter(movieFilter);
+            return Promise.all([
+                deleteMovieById(movieId),
+                deletePresentationsAndRelatedTickets(presentationList),
+            ])
         })
         .then(movie => {
             res.status(204);
@@ -94,4 +103,49 @@ const thereIsNoMovie = movie => {
         return movie.length === 0;
     else
         return movie === null;
+};
+
+const getPresentationListWithFilter = filter => new Promise((resolve, reject) => {
+    Presentation.find(filter)
+        .then(presentations => {
+            if (presentations === null) {
+                reject(errors.presentationNotFound);
+            } else {
+                resolve(presentations);
+            }
+        })
+        .catch(err => {
+            reject(err)
+        });
+});
+
+const deletePresentationsWithFilter = filter => new Promise((resolve, reject) => {
+    Presentation.deleteMany(filter)
+        .then(dbResponse => {
+                resolve(dbResponse);
+        })
+        .catch(err => {
+            reject(err)
+        });
+});
+
+const deleteTicketWithFilter = filter => new Promise((resolve, reject) => {
+    Ticket.deleteMany(filter)
+        .then(dbResponse => {
+            resolve(dbResponse);
+        })
+        .catch(err => reject(err));
+});
+
+const deletePresentationsAndRelatedTickets = presentationsList => {
+    const deleteTicketPromiseArray = [];
+
+    presentationsList.forEach( presentation => {
+        const ticketFilter = {presentation: presentation._id};
+        const presentationFilter = {'_id': new ObjectID(presentation._id)};
+        deleteTicketPromiseArray.push(deleteTicketWithFilter(ticketFilter));
+        deleteTicketPromiseArray.push(deletePresentationsWithFilter(presentationFilter));
+    });
+
+    return Promise.all(deleteTicketPromiseArray);
 };
