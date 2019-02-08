290ee6be6b993ade041c8a05c258d57a6dec2a1a
JuanPabloBeco <juanpablobeco@gmail.com>
2019-02-08 11:24:45 -0300

    Build commit

--- cinema-node/app.js
+++ cinema-node/app.js
@@ -37,14 +37,3 @@ app.use('/', routes(router));
 app.use(errors());
 
 module.exports = app;
-
-// const aws = require('aws-sdk');
-//
-// aws.config.update({
-//     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-//     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-//     region: 'us-east-1' // region of your bucket
-// });
-// // const s3 = new aws.S3();
-// console.log(aws.config);
-// console.log(process.env.AWS_SECRET_ACCESS_KEY);
