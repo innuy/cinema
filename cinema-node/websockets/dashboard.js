87b6574ca6b00739061859645beaf1418adb78d0
Juan Pablo Becoña <juan.becona@innuy.com>
2019-03-06 12:04:49 -0300

    Performance improvement in dashboard query
    The three parts of the dashboard query are run at the same time now replacing the old query chaining.

--- cinema-node/websockets/dashboard.js
+++ cinema-node/websockets/dashboard.js
@@ -9,14 +9,16 @@ const sendDataToDashboardNamespace = async () => {
     let dashboardData;
     let dashboardNamespace = app.dashboardNamespace;
 
-    const topMovies = await dashboardQueries.queryTopMovies(timeRangeInDays, 10);
-    const soldRatio = await dashboardQueries.querySoldRatio(timeRangeInDays);
-    const busyTimes = await dashboardQueries.queryBusyTimes(timeRangeInDays);
+    let dashboardDataArray = await Promise.all([
+        dashboardQueries.queryTopMovies(timeRangeInDays, 10),
+        dashboardQueries.querySoldRatio(timeRangeInDays),
+        dashboardQueries.queryBusyTimes(timeRangeInDays),
+    ]);
 
     dashboardData = new Object();
-    dashboardData.topMovies = topMovies;
-    dashboardData.soldRatio = soldRatio;
-    dashboardData.busyTimes = busyTimes;
+    dashboardData.topMovies = dashboardDataArray[0];
+    dashboardData.soldRatio = dashboardDataArray[1];
+    dashboardData.busyTimes = dashboardDataArray[2];
 
     dashboardNamespace.emit('dashboard', dashboardData);
 };
